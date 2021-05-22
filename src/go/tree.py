78955bbb60488025feadd64c9e1ee51e534b0120
Ariel Carvajal <arie.cbpro@gmail.com>
2021-05-22 12:38:14 -0400

    Add bft algorithm

--- src/go/tree.py
+++ src/go/tree.py
@@ -1,13 +1,46 @@
+from typing import Any, Generator, Optional, List, Union
+
+
 class Node:
     def __init__(
         self,
-        name: str = '',
-        vertex_name: str = ''
+        name: Optional[str] = None,
+        vertex_name: Optional[str] = None,
+        data: Optional[Any] = None,
+        children: Optional[List['Node']] = None
     ):
         self.name = name
         self.vertex_name = vertex_name
 
+        self.data = data
+        self.children = children if children is not None else []
+
+    def add(self, node: 'Node'):
+        self.children.append(node)
+
+    def __str__(self):
+        return f'<Node name:{self.name}>'
+
+    def __repr__(self):
+        return self.__str__()
+
 
 class Tree(Node):
     def __init__(self, *args, **kwargs):
-        super(*args, **kwargs)
+        super().__init__(*args, **kwargs)
+
+    @classmethod
+    def breadth_first_traversal(
+        cls,
+        tree: 'Tree'
+    ) -> Generator[Union['Tree', Node], None, None]:
+        queue: List[Union[Tree, Node]] = [tree]
+
+        while len(queue) != 0:
+            yield from queue
+
+            new = []
+            for e in queue:
+                new.extend(e.children)
+
+            queue = new
--- src/tests/test_tree.py
+++ src/tests/test_tree.py
@@ -1,9 +1,72 @@
-from go.tree import Node
+from itertools import zip_longest
+
+from go.tree import Node, Tree
 
 
-class TestTree:
+class TestNode:
     def test_node_properties(self):
         n = Node()
 
         assert hasattr(n, 'name')
         assert hasattr(n, 'vertex_name')
+        assert hasattr(n, 'children')
+        assert hasattr(n, 'data')
+
+        assert n.name is None
+        assert n.vertex_name is None
+        assert n.children == []
+        assert n.data is None
+
+    def test_node_repr(self):
+        n = Node(
+            'MyNode',
+        )
+
+        assert str(n) == '<Node name:MyNode>'
+        assert repr(n) == '<Node name:MyNode>'
+
+    def test_node_methods(self):
+        n = Node()
+
+        assert hasattr(n, 'add')
+
+        assert callable(n.add)
+
+
+class TestTree:
+    def test_tree_bft(self):
+        t = Tree('root')
+
+        node_list = [t] + [Node(f'{i}') for i in range(21)]
+
+        assert len(node_list) == 22
+
+        node_list[0].add(node_list[1])
+        node_list[0].add(node_list[2])
+        node_list[0].add(node_list[3])
+        node_list[1].add(node_list[4])
+        node_list[1].add(node_list[5])
+        node_list[2].add(node_list[6])
+        node_list[2].add(node_list[7])
+        node_list[3].add(node_list[8])
+        node_list[4].add(node_list[9])
+        node_list[4].add(node_list[10])
+        node_list[6].add(node_list[11])
+        node_list[6].add(node_list[12])
+        node_list[8].add(node_list[13])
+        node_list[8].add(node_list[14])
+        node_list[9].add(node_list[15])
+        node_list[9].add(node_list[16])
+        node_list[12].add(node_list[17])
+        node_list[12].add(node_list[18])
+        node_list[14].add(node_list[19])
+        node_list[19].add(node_list[20])
+        node_list[19].add(node_list[21])
+
+        for i, (node_in_tree, node) in enumerate(
+            zip_longest(
+                Tree.breadth_first_traversal(t),
+                node_list
+            )
+        ):
+            assert node_in_tree is node
